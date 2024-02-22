e8c235ca872756a5196dd02583135ed13dd54ec6
Prasad Sonawane <prasadsonawane101@gmail.com>
2024-02-22 17:10:57 +0530

    final commit

--- portfolio/src/Container/contact/index.jsx
+++ portfolio/src/Container/contact/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import '../contact/contact.css';
-// import p1 from './1.png';
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+  const form = useRef();
+  const [message,setMessage]=useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9w6vyvs', 'template_ak2xiv6', form.current, {
+        publicKey: '2GR1p0h5sLghpO_-A',
+      })
+      .then(
+        (result) => {
+          setMessage('Email Send Successfully !!');
+          console.log("message send successfully");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setMessage(`Erro: ${error.text}`);
+        }
+      );
+    };
   return (
     <div>
       <Helmet>
@@ -25,32 +48,36 @@ const Contact = () => {
             <div className="image-box">
               <img draggable="false" src="https://th.bing.com/th/id/OIP.64JVgSNX0KGCS5w8qvkCXwHaEu?pid=ImgDet&w=206&h=131&c=7&dpr=1.3" alt="" />
             </div>
+            <form ref={form} onSubmit={sendEmail}>
             <div className="contact-form">
               <div className="form-group">
                 <div className="field">
                 <i className="fas fa-user"></i>
-                  <input type="text" name="name" placeholder="Name" required />
+                  <input type="text" name="user_name" placeholder="Name" required />
                   
                 </div>
                 <div className="field">
-                  <input type="text" name="email" placeholder="Email" required />
+                  <input type="text" name="user_email" placeholder="Email" required />
                   <i className="fas fa-envelope"></i>
                 </div>
                 <div className="field">
-                  <input type="text" name="phone" placeholder="Phone" />
+                  <input type="text" name="Mobile_number" placeholder="Phone" />
                   <i className="fas fa-phone-alt"></i>
                 </div>
                 <div className="message">
+                {message && <h2 className={`message ${message.includes("Error") ? "error" : "success"}`}>{message}</h2>}
                   <textarea placeholder="Message" name="message" required></textarea>
                   <i className="fas fa-comment-dots"></i>
                 </div>
               </div>
               <div className="button-area">
-                <button type="submit">
+                <button type="submit" value="send">
                   Submit <i className="fa fa-paper-plane"></i>
                 </button>
               </div>
             </div>
+            </form>
+           
           </div>
         </div>
       </section>
